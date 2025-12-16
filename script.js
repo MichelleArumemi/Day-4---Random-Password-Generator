214f16d0cf5c1d57058d14e1e87ed0d2edf8e4a8
MichelleArumemi <Arumemi.michelle@binghamuni.edu.ng>
2025-12-17 00:12:06 +0100

    added copy function

--- script.js
+++ script.js
@@ -12,11 +12,16 @@ function createPassword(){
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    passwod += numbers[Math.floor(Math.random() * numbers.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
     password += symbols[Math.floor(Math.random() * symbols.length)];
 
     while(length > password.length){
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
     passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
 }
